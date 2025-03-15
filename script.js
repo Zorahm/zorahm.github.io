763572ae4d9c71347d6bb89a5bf3e7cca8f993c4
Zorahm <109484136+Zorahm@users.noreply.github.com>
2025-03-15 14:40:03 +0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const toggleThemeButton = document.getElementById("toggleTheme");
-
-    toggleThemeButton.addEventListener("click", () => {
-        document.body.classList.toggle("light-theme");
-    });
-});
